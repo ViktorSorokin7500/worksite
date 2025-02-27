9fc706e3f7f38ce6fd969b70728065a74836f5a9
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2025-02-27 14:18:14 +0200

    site map site name

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://viktorsorokin7500.github.io/worksite"; // Замени на свой домен
+  const baseUrl = "https://www.sorokinsv.com"; // Замени на свой домен
 
   return [
     {
